68908127bdd7c4e0f4eb0816c1e8616d74a76870
The-_-onE <834489218@qq.com>
2016-06-20 19:31:42 +0800

    Modify the processing logic of SplashActivity

--- app/src/main/java/com/xmx/androidframeworkbase/Constants.java
+++ app/src/main/java/com/xmx/androidframeworkbase/Constants.java
@@ -16,7 +16,8 @@ public class Constants {
     public static final String DATABASE_FILE = DATABASE_DIR + "/database.db";
 
     public static final long LONGEST_EXIT_TIME = 2000;
-    public static final long SPLASH_TIME = 3000;
+    public static final long SPLASH_TIME = 2000;
+    public static final long LONGEST_SPLASH_TIME = 6000;
 
     public static final long UPDATE_FREQUENCY = 1000;
 
--- app/src/main/java/com/xmx/androidframeworkbase/SplashActivity.java
+++ app/src/main/java/com/xmx/androidframeworkbase/SplashActivity.java
@@ -12,49 +12,78 @@ import com.xmx.androidframeworkbase.User.UserManager;
 
 public class SplashActivity extends BaseActivity {
     boolean loginFlag = false;
+    boolean timeFlag = false;
+    boolean notLoginFlag = false;
 
     @Override
     protected void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                timeFlag = true;
                 if (loginFlag) {
-                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(mainIntent);
-                    finish();
-                } else {
-                    Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
-                    startActivity(loginIntent);
-                    finish();
+                    startMainActivity();
+                }
+                if (notLoginFlag) {
+                    startLoginActivity();
                 }
             }
         }, Constants.SPLASH_TIME);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (loginFlag) {
+                    startMainActivity();
+                } else {
+                    startLoginActivity();
+                }
+            }
+        }, Constants.LONGEST_SPLASH_TIME);
 
 
         UserManager.getInstance().autoLogin(new AutoLoginCallback() {
             @Override
             public void success(AVObject user) {
                 loginFlag = true;
+                if (timeFlag) {
+                    startMainActivity();
+                }
             }
 
             @Override
             public void notLoggedIn() {
+                notLoginFlag = true;
+                if (timeFlag) {
+                    startLoginActivity();
+                }
             }
 
             @Override
             public void errorNetwork() {
                 showToast(R.string.network_error);
+                notLoginFlag = true;
+                if (timeFlag) {
+                    startLoginActivity();
+                }
             }
 
             @Override
             public void errorUsername() {
+                notLoginFlag = true;
+                if (timeFlag) {
+                    startLoginActivity();
+                }
             }
 
             @Override
             public void errorChecksum() {
+                notLoginFlag = true;
+                if (timeFlag) {
+                    startLoginActivity();
+                }
             }
         });
     }
@@ -67,5 +96,17 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void processLogic(Bundle savedInstanceState) {
 
+    }
+
+    void startLoginActivity() {
+        Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+
+    void startMainActivity() {
+        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }

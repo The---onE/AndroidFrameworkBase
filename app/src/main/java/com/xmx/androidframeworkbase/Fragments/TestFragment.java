a01002b698ba25cb9094b12ba76dedc6590a4d39
The-_-onE <834489218@qq.com>
2016-06-21 22:43:09 +0800

    Add Manager for IM

--- app/src/main/java/com/xmx/androidframeworkbase/Fragments/TestFragment.java
+++ app/src/main/java/com/xmx/androidframeworkbase/Fragments/TestFragment.java
@@ -5,9 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.im.v2.AVIMClient;
+import com.avos.avoscloud.im.v2.AVIMConversation;
+import com.avos.avoscloud.im.v2.AVIMException;
+import com.avos.avoscloud.im.v2.callback.AVIMClientCallback;
 import com.xmx.androidframeworkbase.R;
 import com.xmx.androidframeworkbase.Tools.BaseFragment;
+import com.xmx.androidframeworkbase.Tools.IM.Callback.CreateConversationCallback;
+import com.xmx.androidframeworkbase.Tools.IM.IMClientManager;
+import com.xmx.androidframeworkbase.User.Callback.AutoLoginCallback;
+import com.xmx.androidframeworkbase.User.UserManager;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +36,62 @@ public class TestFragment extends BaseFragment {
 
     @Override
     protected void setListener(View view) {
+        Button im = (Button) view.findViewById(R.id.btn_im);
+        im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserManager.getInstance().checkLogin(new AutoLoginCallback() {
+                    @Override
+                    public void success(AVObject user) {
+                        showToast("IM客户端打开中……");
+                        final IMClientManager im = IMClientManager.getInstance();
+                        im.openClient(user.getString("username"),
+                                new AVIMClientCallback() {
+                                    @Override
+                                    public void done(AVIMClient avimClient, AVIMException e) {
+                                        showToast("IM客户端打开成功");
+                                        im.createConversation("test", new CreateConversationCallback() {
+                                            @Override
+                                            public void success(AVIMConversation conversation) {
+                                                showToast("创建对话成功");
+                                            }
 
+                                            @Override
+                                            public void failure(Exception e) {
+                                                showToast("创建对话失败");
+                                            }
+
+                                            @Override
+                                            public void clientError() {
+                                                showToast("IM客户端打开失败");
+                                            }
+                                        });
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void notLoggedIn() {
+                        showToast(R.string.not_loggedin);
+                    }
+
+                    @Override
+                    public void errorNetwork() {
+                        showToast(R.string.network_error);
+                    }
+
+                    @Override
+                    public void errorUsername() {
+                        showToast(R.string.username_error);
+                    }
+
+                    @Override
+                    public void errorChecksum() {
+                        showToast(R.string.not_loggedin);
+                    }
+                });
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/xmx/androidframeworkbase/Tools/IM/IMClientManager.java
@@ -0,0 +1,129 @@
+package com.xmx.androidframeworkbase.Tools.IM;
+
+import com.avos.avoscloud.im.v2.AVIMClient;
+import com.avos.avoscloud.im.v2.AVIMConversation;
+import com.avos.avoscloud.im.v2.AVIMConversationQuery;
+import com.avos.avoscloud.im.v2.AVIMException;
+import com.avos.avoscloud.im.v2.callback.AVIMClientCallback;
+import com.avos.avoscloud.im.v2.callback.AVIMConversationCallback;
+import com.avos.avoscloud.im.v2.callback.AVIMConversationCreatedCallback;
+import com.avos.avoscloud.im.v2.callback.AVIMConversationQueryCallback;
+import com.xmx.androidframeworkbase.Tools.IM.Callback.CreateConversationCallback;
+import com.xmx.androidframeworkbase.Tools.IM.Callback.FindConversationCallback;
+import com.xmx.androidframeworkbase.Tools.IM.Callback.JoinConversationCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wli on 15/8/13.
+ */
+public class IMClientManager {
+    private static IMClientManager imClientManager;
+
+    private AVIMClient client;
+    private String username;
+    private boolean openFlag = false;
+
+    public synchronized static IMClientManager getInstance() {
+        if (null == imClientManager) {
+            imClientManager = new IMClientManager();
+        }
+        return imClientManager;
+    }
+
+    boolean checkClient() {
+        return openFlag && client != null && username != null && username.length() > 0;
+    }
+
+    public void openClient(String username, AVIMClientCallback callback) {
+        this.username = username;
+        client = AVIMClient.getInstance(username);
+        client.open(callback);
+        openFlag = true;
+    }
+
+    public void closeClient(AVIMClientCallback callback) {
+        if (checkClient()) {
+            client.close(callback);
+            openFlag = false;
+            client = null;
+            username = "";
+        }
+    }
+
+    public void createConversation(String name, final CreateConversationCallback callback) {
+        if (checkClient()) {
+            client.createConversation(new ArrayList<String>(), name, null,
+                    new AVIMConversationCreatedCallback() {
+                        @Override
+                        public void done(AVIMConversation avimConversation, AVIMException e) {
+                            if (e == null) {
+                                callback.success(avimConversation);
+                            } else {
+                                callback.failure(e);
+                            }
+                        }
+                    });
+        } else {
+            callback.clientError();
+        }
+    }
+
+    public void findConversation(String name, final FindConversationCallback callback) {
+        if (checkClient()) {
+            AVIMConversationQuery query = client.getQuery();
+            query.whereEqualTo("name", name);
+            query.findInBackground(new AVIMConversationQueryCallback() {
+                @Override
+                public void done(List<AVIMConversation> convs, AVIMException e) {
+                    if (e == null) {
+                        if (convs != null && !convs.isEmpty()) {
+                            callback.found(convs.get(0));
+                        } else {
+                            callback.notFound();
+                        }
+                    } else {
+                        callback.error(e);
+                    }
+                }
+            });
+        } else {
+            callback.clientError();
+        }
+    }
+
+    public void joinConversation(final AVIMConversation conversation, final JoinConversationCallback callback) {
+        if (checkClient()) {
+            if (!conversation.getMembers().contains(username)) {
+                conversation.join(new AVIMConversationCallback() {
+                    @Override
+                    public void done(AVIMException e) {
+                        if (e == null) {
+                            callback.success(conversation);
+                        } else {
+                            callback.failure(e);
+                        }
+                    }
+                });
+            } else {
+                callback.success(conversation);
+            }
+        } else {
+            callback.clientError();
+        }
+    }
+
+    public void quitConversation(AVIMConversation conversation) {
+        if (checkClient() && conversation != null) {
+            conversation.quit(new AVIMConversationCallback() {
+                @Override
+                public void done(AVIMException e) {
+                    if (e != null) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}

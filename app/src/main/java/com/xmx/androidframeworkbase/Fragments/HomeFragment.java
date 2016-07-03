4b53476dc25f86103d97dcbd6b5bda6e21caf22c
The-_-onE <834489218@qq.com>
2016-07-03 10:55:31 +0800

    Fit bug that can't Stop Service if Activity been Destroyed

--- app/src/main/java/com/xmx/androidframeworkbase/Fragments/HomeFragment.java
+++ app/src/main/java/com/xmx/androidframeworkbase/Fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.xmx.androidframeworkbase.Fragments;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,6 +16,8 @@ import com.xmx.androidframeworkbase.Services.MainService;
 import com.xmx.androidframeworkbase.Tools.FragmentBase.BaseFragment;
 import com.xmx.androidframeworkbase.Tools.Notification.NotificationTempActivity;
 import com.xmx.androidframeworkbase.Tools.Notification.NotificationUtils;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,6 +57,7 @@ public class HomeFragment extends BaseFragment {
                 intent.putExtra("notificationId", i);
 
                 NotificationUtils.showNotification(getContext(), intent, i, title, content);
+                showToast("已发送通知");
             }
         });
 
@@ -68,7 +73,9 @@ public class HomeFragment extends BaseFragment {
                 Intent intent = new Intent(getContext(), NotificationTempActivity.class);
                 intent.putExtra("notificationId", i);
 
-                NotificationUtils.showNotification(getContext(), intent, i, title, content, true, true);
+                NotificationUtils.showNotification(getContext(),
+                        intent, i, title, content, true, true);
+                showToast("已发送持续通知");
             }
         });
 
@@ -79,6 +86,7 @@ public class HomeFragment extends BaseFragment {
                 String id = idView.getText().toString();
                 int i = id.hashCode();
                 NotificationUtils.removeNotification(getContext(), i);
+                showToast("已移除通知");
             }
         });
 
@@ -88,6 +96,7 @@ public class HomeFragment extends BaseFragment {
             public void onClick(View view) {
                 service = new Intent(getContext(), MainService.class);
                 getContext().startService(service);
+                showToast("已开启服务");
             }
         });
 
@@ -95,8 +104,27 @@ public class HomeFragment extends BaseFragment {
         endService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (service != null) {
-                    getContext().stopService(service);
+                ActivityManager manager =
+                        (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+                int defaultNum = 1000;
+                List<ActivityManager.RunningServiceInfo> runServiceList = manager
+                        .getRunningServices(defaultNum);
+                boolean flag = false;
+                for (ActivityManager.RunningServiceInfo runServiceInfo : runServiceList) {
+                    if (runServiceInfo.foreground) {
+                        if (runServiceInfo.service
+                                .getShortClassName().equals(".Services.MainService")) {
+                            Intent intent = new Intent();
+                            intent.setComponent(runServiceInfo.service);
+                            getContext().stopService(intent);
+                            showToast("已关闭服务");
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                if (!flag) {
+                    showToast("服务未开启");
                 }
             }
         });

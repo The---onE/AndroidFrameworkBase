583521ec13e3932cf040850a22ecc9a1f3a1c4dd
The-_-onE <834489218@qq.com>
2016-08-09 11:02:14 +0800

    Could Save QR Code to a jpeg File

--- app/src/main/java/com/xmx/androidframeworkbase/QRCode/CreateQRCodeActivity.java
+++ app/src/main/java/com/xmx/androidframeworkbase/QRCode/CreateQRCodeActivity.java
@@ -2,6 +2,7 @@ package com.xmx.androidframeworkbase.QRCode;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,6 +14,13 @@ import com.xmx.androidframeworkbase.Tools.NewThread;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import cn.bingoogolapple.qrcode.zxing.QRCodeEncoder;
 
@@ -29,8 +37,56 @@ public class CreateQRCodeActivity extends BaseTempActivity {
     private void onClickCreateQR(View view) {
         final String content = contentEdit.getText().toString();
 
-        Bitmap image = QRCodeEncoder.syncEncodeQRCode(content, 500);
-        qrImage.setImageBitmap(image);
+        if (!content.equals("")) {
+            Bitmap image = QRCodeEncoder.syncEncodeQRCode(content, 500);
+            qrImage.setImageBitmap(image);
+        } else {
+            showToast("内容不能为空");
+        }
+    }
+
+    @Event(value = R.id.btn_save_qr)
+    private void onClickSaveQR(View view) {
+        final String content = contentEdit.getText().toString();
+
+        if (!content.equals("")) {
+            Bitmap image = QRCodeEncoder.syncEncodeQRCode(content, 500);
+            qrImage.setImageBitmap(image);
+
+            if (image == null) {
+                showToast("生成二维码失败");
+                return;
+            }
+            Bitmap.CompressFormat format = Bitmap.CompressFormat.JPEG;
+            int quality = 100;
+            OutputStream stream;
+            try {
+                File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES), "Camera");
+
+                if (!mediaStorageDir.exists()) {
+                    if (!mediaStorageDir.mkdirs()) {
+                        showToast("创建目录失败");
+                        return;
+                    }
+                }
+
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+                String filepath = mediaStorageDir.getPath() + File.separator +
+                        "IMG_" + timeStamp + ".jpg";
+
+                stream = new FileOutputStream(filepath);
+
+                image.compress(format, quality, stream);
+
+                showToast("图片保存至" + filepath);
+            } catch (FileNotFoundException e) {
+                filterException(e);
+            }
+        } else {
+            showToast("内容不能为空");
+        }
     }
 
     @Override

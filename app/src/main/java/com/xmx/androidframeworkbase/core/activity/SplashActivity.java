b077a90a5bf0cb0a93edda98f5d52e4e60edcc54
Xu Mengxiang <834489218@qq.com>
2017-03-08 15:59:21 +0800

    Fix bug that show White screen before SplashActivity

--- app/src/main/java/com/xmx/androidframeworkbase/core/activity/SplashActivity.java
+++ app/src/main/java/com/xmx/androidframeworkbase/core/activity/SplashActivity.java
@@ -2,32 +2,42 @@ package com.xmx.androidframeworkbase.core.activity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import com.xmx.androidframeworkbase.core.Constants;
 import com.xmx.androidframeworkbase.R;
 import com.xmx.androidframeworkbase.base.activity.BaseSplashActivity;
+import com.xmx.androidframeworkbase.utils.Timer;
 
 public class SplashActivity extends BaseSplashActivity {
+
+    Timer timer;
 
     @Override
     protected void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_splash);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startMainActivity();
-            }
-        }, Constants.SPLASH_TIME);
     }
 
     @Override
     protected void setListener() {
-
+        getViewById(R.id.btn_skip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.stop();
+                timer.execute();
+            }
+        });
     }
 
     @Override
     protected void processLogic(Bundle savedInstanceState) {
+        timer = new Timer() {
+            @Override
+            public void timer() {
 
+                startMainActivity();
+            }
+        };
+        timer.start(Constants.SPLASH_TIME, true);
     }
 }
--- app/src/main/java/com/xmx/androidframeworkbase/utils/Timer.java
+++ app/src/main/java/com/xmx/androidframeworkbase/utils/Timer.java
@@ -7,21 +7,29 @@ import android.os.Handler;
  */
 public abstract class Timer {
     long delay = 1000;
+    boolean onceFlag = false;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
             timer();
-            handler.postDelayed(this, delay);
+            if (!onceFlag) {
+                handler.postDelayed(this, delay);
+            }
         }
     };
 
     public abstract void timer();
 
-    public void start(long d) {
+    public void start(long d, boolean once) {
         delay = d;
+        onceFlag = once;
         handler.postDelayed(runnable, delay);
+    }
+
+    public void start(long d) {
+        start(d, false);
     }
 
     public void stop() {
